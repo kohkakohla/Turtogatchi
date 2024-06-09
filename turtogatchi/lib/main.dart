a27e5a3f3e4a9c269db0bffe28750408c6a3d447
Blerber <marthayeo.pn@gmail.com>
2024-06-10 00:00:10 +0800

    implemented url redirects for museum page, not sure if works

--- turtogatchi/lib/main.dart
+++ turtogatchi/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio_background/just_audio_background.dart';
 import 'package:turtogatchi/firebase_options.dart';
 import 'package:turtogatchi/forgot_password.dart';
 import 'package:turtogatchi/home.dart';
@@ -7,10 +6,14 @@ import 'package:turtogatchi/sign_up.dart';
 import 'package:turtogatchi/sign_up_email.dart';
 import 'package:turtogatchi/splash.dart';
 import 'package:turtogatchi/login.dart';
-
+import 'package:flutter/material.dart';
+import 'package:turtogatchi/home.dart';
+import 'package:turtogatchi/splash.dart';
+import 'package:turtogatchi/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 void main() async {
   WidgetsFlutterBinding
       .ensureInitialized(); // Ensure Flutter bindings are initialized
@@ -31,32 +34,9 @@ void main() async {
 }
 
 
-
-void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure Flutter bindings are initialized
-  
-  // initialize firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  // Initialize Google Mobile Ads
-  await MobileAds.instance.initialize();
-
-  // Sign out the user when launching for TESTing purposes
-  await FirebaseAuth.instance.signOut();
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  MyAppState createState() => MyAppState();
-}
 
-class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -67,9 +47,9 @@ class MyAppState extends State<MyApp> {
         ),
         initialRoute: '/splash',
         routes: {
-          '/splash': (context) => SplashScreen(),
+          '/splash': (context) => const SplashScreen(),
           '/login': (context) => LoginPage(),
-          '/home': (context) => HomePage(),
+          '/home': (context) => const HomePage(),
           '/sign_up': (context) => SignUpPage(),
           '/sign_up_email': (context) => SignUpEmailPage(),
           '/forgot_password': (context) => ForgotPasswordPage(),
