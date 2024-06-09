4f225cf5fb1fdd0a2bd43c754ad1c2343c14747a
Shaun <Hundin231@gmail.com>
2024-06-09 18:18:51 +0800

    added looping background music on app start up

--- turtogatchi/lib/main.dart
+++ turtogatchi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:turtogatchi/firebase_options.dart';
 import 'package:turtogatchi/forgot_password.dart';
 import 'package:turtogatchi/home.dart';
@@ -6,28 +7,30 @@ import 'package:turtogatchi/sign_up.dart';
 import 'package:turtogatchi/sign_up_email.dart';
 import 'package:turtogatchi/splash.dart';
 import 'package:turtogatchi/login.dart';
-import 'package:flutter/material.dart';
-import 'package:turtogatchi/home.dart';
-import 'package:turtogatchi/splash.dart';
-import 'package:turtogatchi/login.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure Flutter bindings are initialized
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Ensure Flutter bindings are initialized
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   // Sign out the user when launching for TESTing purposes
   await FirebaseAuth.instance.signOut();
+
   runApp(const MyApp());
-  print('');
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  MyAppState createState() => MyAppState();
+}
 
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,9 +41,9 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/splash',
         routes: {
-          '/splash': (context) => const SplashScreen(),
+          '/splash': (context) => SplashScreen(),
           '/login': (context) => LoginPage(),
-          '/home': (context) => const HomePage(),
+          '/home': (context) => HomePage(),
           '/sign_up': (context) => SignUpPage(),
           '/sign_up_email': (context) => SignUpEmailPage(),
           '/forgot_password': (context) => ForgotPasswordPage(),
