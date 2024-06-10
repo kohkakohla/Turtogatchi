b04aaaf0c939ad9b1b6b9ad9f33a0641a7df2ed2
Ivan <ivanjerrickkoh@gmail.com>
2024-06-10 22:02:28 +0800

    Added ad functionality

--- turtogatchi/macos/Flutter/GeneratedPluginRegistrant.swift
+++ turtogatchi/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import firebase_auth
 import firebase_core
 import google_sign_in_ios
 import path_provider_foundation
-import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,7 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-
 }
