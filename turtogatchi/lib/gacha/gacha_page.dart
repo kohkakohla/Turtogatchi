b976093f88f6796365bf42e5ff64bd48dab0d37a
Ivan <ivanjerrickkoh@gmail.com>
2024-06-11 18:49:17 +0800

    Small bug fixes, UI fixes to inventory, encyclopedia

--- turtogatchi/lib/gacha/gacha_page.dart
+++ turtogatchi/lib/gacha/gacha_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -56,8 +58,6 @@ class GachaPageState extends State<GachaPage> with TickerProviderStateMixin {
     } else {
       print("User data does not exist!");
     }
-    print("Coins: $coins");
-    print("Inventory: $inventory");
   }
 
   void updateCoinsBackend() async {
